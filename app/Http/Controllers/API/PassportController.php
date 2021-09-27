9b84335c620baf128728169e67be901c6d07acd3
Akash Anil <77885193+akashanilz@users.noreply.github.com>
2021-09-27 22:15:37 +0530

    oo

--- app/Http/Controllers/API/PassportController.php
+++ app/Http/Controllers/API/PassportController.php
@@ -809,9 +809,9 @@ else{
     if(auth()->user()){
         $roles=UserRole::where('user_id','=',auth()->user()->id)->first();
         if($roles->role == 1  || $roles->role == 2 ){
-            $appointment= Group::where('appointment_id','=',$id)->with('appointment','client')->get();
+            $group= Group::where('appointment_id','=',$id)->with('appointment','client')->get();
             $employee=Appointment::find($id)->employee;
-            $group=Appointment::find($id);
+            $appointment=Appointment::with('id','=',$id)->with('time')->get();
             $driver=Appointment::find($id)->driver;
             return response()->json(['appointment'=>$appointment,'group'=>$group,'employee'=>$employee,'driver'=>$driver],200);
         }
